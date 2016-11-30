f7ceb66e6ece18bbc8f79ceab5e4555b5376aa31
saurabh19 <code.saurabh19@gmail.com>
2016-11-30 20:21:20 +0530

    Yeah its working !!

--- src/main/webapp/resources/systemjs.config.js
+++ src/main/webapp/resources/systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'app',
+      app: '/resources/app',
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -29,7 +29,7 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: '/resources/app/main.js',
+        main: './main.js',
         defaultExtension: 'js'
       },
       rxjs: {
