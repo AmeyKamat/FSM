5df14e3a66451ed9623dc065f5ae24ee73e91212
Rohit Singh <rohitsingh17219@gmail.com>
2016-12-06 02:22:07 +0530

    security level 2

--- src/main/java/fsm/controller/LoginController.java
+++ src/main/java/fsm/controller/LoginController.java
@@ -9,25 +9,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 
+	// Spring Security see this :
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
 
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
-    public ModelAndView showLoginPage() {
-        return new ModelAndView("login.html");
-    }
-    
-    @RequestMapping(method = RequestMethod.POST)
-    @ResponseBody
-    public ModelAndView authenticateAndLogin() {
-        return new ModelAndView("redirect: /controller/dummyLogout");
-    }
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		
+		model.setViewName("login.jsp");
+
+		return model;
+
+	}
 
 }
--- src/main/webapp/resources/dummyLogout.jsp
+++ src/main/webapp/resources/dummyLogout.jsp
@@ -10,7 +10,8 @@
 		<c:url var="logoutUrl" value="/logout" />
 		<form action="${logoutUrl}" method="post">
 			<input type="submit" value="Log out" /> <input type="hidden"
-				name="${_csrf.parameterName}" value="${_csrf.token}" />
+				name="${_csrf.parameterName}" value="${_csrf.token}" /> 
+			<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
 		</form>
 	</c:if>
 </body>
