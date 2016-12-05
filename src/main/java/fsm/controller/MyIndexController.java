730276ad31692dcfc69abd0f934b744b38987e14
Rohit Singh <rohitsingh17219@gmail.com>
2016-12-06 00:03:24 +0530

    security level 1

--- src/main/java/fsm/controller/MyIndexController.java
+++ src/main/java/fsm/controller/MyIndexController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/dummyLogout")
+@RequestMapping("/dummyLogout**")
 public class MyIndexController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String showLogoutPage(ModelMap map) {
 
-        return "dummyLogout.html";
+        return "dummyLogout.jsp";
     }
 
 }
