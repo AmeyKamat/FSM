699ed2192c3332cd0d4faa312b40f0516ecfd16f
Rohit Singh <rohitsingh17219@gmail.com>
2016-12-08 12:54:05 +0530

    simple login logout working

--- src/main/java/fsm/controller/AdminController.java
+++ src/main/java/fsm/controller/AdminController.java
@@ -3,16 +3,19 @@ package fsm.controller;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AdminController {
 
 	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
-	public String adminPage(Model model) {
+	public String adminPage(HttpServletRequest request, Model model) {
 		if (!(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)) {
 			User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 			String name = user.getUsername();
@@ -22,6 +25,6 @@ public class AdminController {
 		
 		model.addAttribute("message", "Admin Page (Through Model)");
 		
-		return "admin";
+		return "index";
 	}
 }
--- src/main/java/fsm/controller/WelcomeController.java
+++ src/main/java/fsm/controller/WelcomeController.java
@@ -3,16 +3,20 @@ package fsm.controller;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.HttpServletBean;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class WelcomeController {
 
 	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
-	public String defaultPage(Model model) {
+	public String defaultPage(HttpServletRequest request, Model model) {
 		if (!(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)) {
 			User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 			String name = user.getUsername();
