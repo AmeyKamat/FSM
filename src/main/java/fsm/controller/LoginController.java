7a33872f3e25a7ba3cc9b4a9ddf7c6f92e34097b
Rohit Singh <rohitsingh17219@gmail.com>
2016-12-13 19:44:31 +0530

    password hashed saved on backend. front end hashing remaining

--- src/main/java/fsm/controller/LoginController.java
+++ src/main/java/fsm/controller/LoginController.java
@@ -1,5 +1,6 @@
 package fsm.controller;
 
+import fsm.util.HashGenerator;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
